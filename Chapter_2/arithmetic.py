86dba0840cf7fd5df713d4049447d27694dd82be
Jonathan Heard <jonathan.l.heard@gmail.com>
2021-11-02 21:38:22 -0700

    Created an Average function and then researched how to enter a list to fill the average function.

--- Chapter_2/arithmetic.py
+++ Chapter_2/arithmetic.py
@@ -6,7 +6,7 @@
 # Define a function to calculate the average of 2 numbers
 
 
-def average(a, b):
+def average1(a, b):
 
     return (a + b) / 2
 
@@ -14,7 +14,7 @@ def average(a, b):
 # End of function definition
 # Use function to calculate an average
 
-print("Average of 10 and 20 is", average(10, 20))
+print("Average of 10 and 20 is", average1(10, 20))
 
 # Define a new function to use a loop for summation
 
@@ -28,10 +28,53 @@ def mysum(num):
 
 # End of definition
 
-# Request a number so sum to
+# Define a new function n**2 + 1
+
+
+def mysum2(num):
+    running_sum = 0
+    for i in range(num+1):
+        running_sum += i**2 + 1
+    return running_sum
+
+
+# End of definition
+
+
+# Define a new function to average a list of numbers
+
+
+def average3(numlist):
+    return sum(numlist)/int(len(numlist))
+
+
+# End of definition
+
+
+# Request the final number for the summation
 
 num = int(input("Enter the number to sum to: "))
 
-Summation = mysum(num)
+# Print the results of simple summation
 
-print("Results of summing 1 to", num, "is", Summation)
+print("Results of summing 1 to", num, "is", mysum(num))
+
+# Print the results of summing (i^2 + 1)
+
+print("Result of summing (i^2 + 1) from 0 to", num, "is", mysum2(num))
+
+# Using a try block to handle entering integers for a list,
+#   exit the enter when an exception occurs.
+try:
+    lista = []
+
+    while True:
+        lista.append(int(input("Enter Numbers, blank to end: ")))
+
+except:
+
+    # if the input is not-integer, then print the list and the average
+
+    print("The average of", lista, "is",  average3(lista))
+
+# End of program
