9dca8699ad2177c3519756427919e1a8575fb7c0
Jonathan Heard <jonathan.l.heard@gmail.com>
2021-11-11 21:23:55 -0700

    Revised the Greatest Common Factor script to use a tuple for the returned values and then printed them out.

--- Chapter_3/GCFactor.py
+++ Chapter_3/GCFactor.py
@@ -9,8 +9,8 @@
 
 
 def gcf(num1, num2):
-    """ returns a list of common factors for num(1 & 2)
-        as well as the Greatest Common Factor(GCF) """
+    """ returns a list of Common Factors for num(1 & 2)
+            as well as the Greatest Common Factor(GCF) """
     factorlist = []
     gcfactor = 0
     # Determine which number is largest and use it the for loop limit
@@ -20,39 +20,17 @@ def gcf(num1, num2):
         looper = num2
 
     for i in range(2, looper+1):  # go from 2 to looper + 1, since all numbers
-        #   are divisible by 1
-        if num1 % i == 0 and num2 % i == 0:       # if i divides into either numbers
+        #   are divisible by 1 start at 2
+        if num1 % i == 0 and num2 % i == 0:       # if i divides into both numbers
             # evenly its a factor, so add it to the list
             factorlist.append(i)
-            # if i is greater that the current GCF, update GFF
+            # if i is greater that the current GCF (gcfactor), update GCF
             if i > int(gcfactor):
                 gcfactor = i
-    """# Rewriting the end of function to use a tuple
+    # Rewriting the end of function to use a tuple
 
-    factors = (factorlist, gcfactor)
+    return factorlist, gcfactor
 
-    return factors """
-
-    # This was commented out, since I couldn't figure out how to split the
-    #   2 returned values.
-    # finally return the 2 factor lists
-    # return factorlist,gcfactor
-    #
-    # This was moved from the function call section, since I could figure out
-    #   how to split 2 returned values. I simply added to print section section
-    #   to the function.
-
-    #If there is no GCF, then there are not common Factors as well
-    if gcfactor == 0:
-        print("\nThere are no common Factors and no GCF!")
-    else:
-        # added test to control the (s) at the end of factor
-        if len(factorlist) == 1:
-            print("\nThe common Factor is:", factorlist)
-        else:
-            print("\nThe common Factors are:", factorlist)
-        # Print out the GCF
-        print("\nThe Greatest Common Factor (GCF) is:", gcfactor)
 
 # End of function definition
 #
@@ -67,19 +45,16 @@ for i in range(0, 2):
 
 print("\nThe 2 numbers entered were:", lst)
 
-gcf(lst[0], lst[1])
+# Call the function and create a Tuple for the results
 
-"""# Call the gcf function to execute it.
-    Commented out, until I can figure out how to use 
-    tuple function
-factors2 = [gcf(lst[0], lst[1])]
-print(factors2)
-"""
+GCF = gcf(lst[0], lst[1])
 
+# Print the results of the returned tuple
 
-#print("\nThe common factors are:", factors2(0))
-#print("\n\tand the GCF is :", factors2(1))
+if len(GCF[0]) == 1:
+    print("\nThe common Factor is:", GCF[0])
+else:
+    print("\nThe common Factors are:", GCF[0])
 
-# Since I couldn't figure out to split the 2 returned values, I move the print
-#   function inside the actual gcf function
-# print("\nThe common factors are in []'s and the GCF is after the ,:", gcf(pass1, pass2))
+print("\n\tThe Greatest Common Factor is:", GCF[1])
+
