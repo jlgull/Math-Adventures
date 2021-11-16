f6efb6fc7e1c22807999defbda1cc7120da7da17
Jonathan Heard <jonathan.l.heard@gmail.com>
2021-11-15 22:15:35 -0700

    Revised numberGame.py to loop to conclusion. Also changed the permissions for all the *.py files in the project.

--- Chapter_3/numberGame.py
+++ Chapter_3/numberGame.py
@@ -17,32 +17,40 @@ def greet():
     if name == "Jonathan":
         print("Hey, that's my name!")
     else:
-        print("Hello", name)
+        print("Hello", name.title())
     return name
 
 
 def numberGame():
     # Pick a random number between 1 and 100
-    number = randint(1,100)
+    number = randint(1, 100)
     # return number
 
 # Get a user's guess for the number being thought of
 
-    print("\n", greet(), " I'm thinking of a number between 1 and 100.")
+    player = greet()
+    print(player.title(), ", I'm thinking of a number between 1 and 100.")
     guess = int(input("What is your guess? "))
+    attempt = 1
 
 # test the results of the guess against the random number
 
-    if number == guess:
-        print("\nThat is correct! The number was", number)
-    elif number > guess:
-        print("\nNope! Your guess was to low.")
-    else:
-        print("\nNope! Your guess was to high.")
+# begin loop to ask until the guess is correct
+    while guess:
+        if number == guess:
+            print("\n", player.title(), "that is correct! The number was", number)
+            print("\t You guessed it right in", attempt, "trys.")
+            break
+        elif number > guess:
+            print("Nope! Try higher.")
+        else:
+            print("Nope! Try lower.")
+        guess = int(input("What is your new guess? "))
+        attempt += 1
 
+# End of Definition Section
 
 # Playing the game, using the function
-
 
 numberGame()
 
